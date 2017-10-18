1c0f245dc30bcfc9dd21a822547f1d72b6eb37f2
petarnyagolov <petarmiroslavovnqgolov@abv.bg>
2017-10-19 02:26:49 +0300

    begin of example

---
+++ config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+const path = 'mongodb://localhost:27017/imagesDb'
+mongoose.Promise = global.Promise
+
+module.exports = mongoose.connect(path,{
+    useMongoClient:true
+})
